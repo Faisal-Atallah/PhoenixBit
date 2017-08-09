b4d22fe27f8b805849bb0d4dff540119736a5872
Faisal-Atallah <faisal.attallah@hotmail.com>
2017-08-09 11:35:56 -0400

    First commit

---
+++ PhoenixBit/application/controllers/Home.php
@@ -0,0 +1,26 @@
+<?php
+
+class Home extends CI_Controller
+
+{
+
+
+
+
+
+    public function index(){
+
+
+        $this->load->view('header');
+        $this->load->view('body');
+        $this->load->view('footer');
+
+
+
+    }
+
+
+
+
+
+}
---
+++ PhoenixBit/application/views/header.php
@@ -0,0 +1,40 @@
+ <html>
+
+ <head>
+
+     <title>PhoenixBit</title>
+
+
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+     <link rel="stylesheet" href="<?php echo base_url()?>assets/css/steps.css" />
+
+ </head>
+
+
+
+ <body>
+ <nav class="navbar navbar-inverse">
+     <div class="container-fluid">
+         <div class="navbar-header">
+             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+             </button>
+             <a class="navbar-brand" href="#">PhoenixBit</a>
+         </div>
+         <div class="collapse navbar-collapse" id="myNavbar">
+             <ul class="nav navbar-nav">
+                 <li class="active"><a href="#">Home</a></li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">FAQ</a></li>
+
+             </ul>
+         </div>
+     </div>
+ </nav>
+
+
+
+
